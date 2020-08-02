8a3345d0844e57553772c6481b7ce46b480f6e3f
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-08-02 14:57:41 +0500

    Изменено средство хранения строки в тестах

--- taxiModel_test/taxiContext_test.cs
+++ taxiModel_test/taxiContext_test.cs
@@ -1,29 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using taxiModel;
-using System.Configuration;
 
 namespace taxiModel_test
 {
     [TestClass]
     public class taxiContext_test
     {
+        public void init()
+        {
+
+        }
+
         [TestMethod]
         public void testCteate()
         {
             taxiContext tc = new taxiContext();
         }
 
+
         [TestMethod]
         public void SetConnectionString()
         {
-            string ConnectionString = ConfigurationManager.ConnectionStrings["defaultString"].ConnectionString;
+            string ConnectionString = Settings1.Default.ts;
             taxiContext.SetConnectionString(ConnectionString);
         }
 
         [TestMethod]
         public void CheckConnectionString()
         {
-            string ConnectionString = ConfigurationManager.ConnectionStrings["defaultString"].ConnectionString;
+            string ConnectionString = Settings1.Default.ts;
             taxiContext.SetConnectionString(ConnectionString);
             Assert.AreEqual(ConnectionString, taxiContext.GetConnectionString());
         }
