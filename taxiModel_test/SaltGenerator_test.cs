c31443e1668da7ee952a014836d8339f265b16b2
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2020-08-09 15:46:58 +0500

    Доделаны и проведены все тесты для SaltGenerator

--- taxiModel_test/SaltGenerator_test.cs
+++ taxiModel_test/SaltGenerator_test.cs
@@ -63,5 +63,54 @@ namespace taxiModel_test
             }
         }
 
+        [TestMethod]
+        public void CheckDifferentPass()
+        {
+            SaltGenerator saltGenerator = new SaltGenerator(), saltGenerator1 = new SaltGenerator();
+            byte[] hash = null, salt = null, hash2 = null;
+            saltGenerator.GetOpenPassword(OpenPassword, out hash, out salt);
+            hash2 = saltGenerator1.GetHash(OpenPassword, salt);
+            for (int i = 0; i < saltGenerator.GetSize; i++)
+            {
+                if (hash[i] != hash2[i]) Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void CheckStringHashSaltPass()
+        {
+            SaltGenerator saltGenerator = new SaltGenerator();
+            string hash = "", salt = "";
+            saltGenerator.GetOpenPassword(OpenPassword, out hash, out salt);
+        }
+
+        [TestMethod]
+        public void CheckGeStringtHas()
+        {
+            SaltGenerator saltGenerator = new SaltGenerator();
+            string hash = "", salt = "";
+            saltGenerator.GetOpenPassword(OpenPassword, out hash, out salt);
+            string hash2 = saltGenerator.GetHash(OpenPassword, salt);
+        }
+
+        [TestMethod]
+        public void CheckStringEqualPass()
+        {
+            SaltGenerator saltGenerator = new SaltGenerator();
+            string hash = "", salt = "";
+            saltGenerator.GetOpenPassword(OpenPassword, out hash, out salt);
+            string hash2 = saltGenerator.GetHash(OpenPassword, salt);
+            Assert.AreEqual(hash, hash2);
+        }
+
+        [TestMethod]
+        public void CheckDifferentEqualPass()
+        {
+            SaltGenerator saltGenerator = new SaltGenerator(), saltGenerator1 = new SaltGenerator();
+            string hash = "", salt = "";
+            saltGenerator.GetOpenPassword(OpenPassword, out hash, out salt);
+            string hash2 = saltGenerator1.GetHash(OpenPassword, salt);
+            Assert.AreEqual(hash, hash2);
+        }
     }
 }
